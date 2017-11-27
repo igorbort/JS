8ef0ac40dcf659891cbfe59e086b0d0ee969e4da
bort <igor.bortnikov32@gmail.com>
2017-11-27 14:06:05 +0200

    Task #1: Sum

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
-var $ = jQuery;
-$(function () {
 
 
-    //Changing btn name
-    $('.sw_btn').click(function () {
-        $(this).text(function (i, text) {
-            return text === 'Company' ? 'Individual' : 'Company';
-        });
-    });
+var i = 1;
+var sum = 0;
+while (i <= 100) {
+    i++;
+    sum += i;
+}
 
-    //Active elements css
-    $('.pro').click(function () {
-        $(this).addClass('active');
-        $('.starter').removeClass('active');
-    });
-    //Active elements css
-    $('.starter').click(function () {
-        $(this).addClass('active');
-        $('.pro').removeClass('active');
-    });
-    //Hamburger add class
-    $('#nav-icon1').click(function () {
-        $(this).toggleClass('open');
-        $('.mobile-nav').toggleClass('d-block');
-    });
-    //Slow scroll
-    $('.watch-video').click(function () {
-        $('html, body').animate({scrollTop: $('section.first-section').offset().top}, 1500);
-        return false;
-    });
+console.log(sum);
 
-});
+var result=0;
+for (var i = 1; i <= 100; i++)
+{
+    result = result + i;
+}
+console.log(result);
