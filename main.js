a8b44e445eaeab2d55a1a70e151fc84ee1cb5f08
bort <igor.bortnikov32@gmail.com>
2017-11-29 16:38:16 +0200

    Task #2: Second hometask.

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-var $ = jQuery;
-$(function () {
-
-
-    //Changing btn name
-    $('.sw_btn').click(function () {
-        $(this).text(function (i, text) {
-            return text === 'Company' ? 'Individual' : 'Company';
-        });
-    });
-
-    //Active elements css
-    $('.pro').click(function () {
-        $(this).addClass('active');
-        $('.starter').removeClass('active');
-    });
-    //Active elements css
-    $('.starter').click(function () {
-        $(this).addClass('active');
-        $('.pro').removeClass('active');
-    });
-    //Hamburger add class
-    $('#nav-icon1').click(function () {
-        $(this).toggleClass('open');
-        $('.mobile-nav').toggleClass('d-block');
-    });
-    //Slow scroll
-    $('.watch-video').click(function () {
-        $('html, body').animate({scrollTop: $('section.first-section').offset().top}, 1500);
+// First task
+function sum(a, b) {
+    if (b > a) {
+        var result = 0;
+        for (var i = a; i <= b; i++) {
+            if (i % 2 === 0 && i % 3 === 0) {
+                result += i;
+            }
+        }
+        return result;
+    } else {
         return false;
-    });
+    }
+}
 
-});
+var test = sum(20, 0);
+console.log(test);
+
+// Second task
+
+var second = function () {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++)
+        sum += arguments[i]
+    return sum
+};
+
+var s = second(20, 10, 30);
+console.log(s);
+
+// Third task
+
+var recursion = function (i, max) {
+    if (i < max) {
+        return i + recursion(i + 1, max);
+    } else {
+        return i;
+    }
+};
+
+var ss = recursion(0, 100);
+console.log(ss);
